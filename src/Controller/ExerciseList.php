5f7ddbcd2d686282f2d8c0a005750640f1c70115
ericiov <eric.iov03@e-uvt.ro>
2024-07-01 20:31:18 +0300

    styiling:)

--- src/Controller/ExerciseList.php
+++ src/Controller/ExerciseList.php
@@ -2,10 +2,15 @@
 namespace App\Controller;
 
 
+use App\Entity\Exercise;
+use App\Entity\Tip;
+use App\Form\Type\ExerciseType;
 use App\Repository\ExerciseRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class ExerciseList extends AbstractController
 {
@@ -16,4 +21,45 @@ class ExerciseList extends AbstractController
         return $this->render('Exercises\exercise_list.html.twig', ['exercises' => $exercises]);
 
     }
+
+    #[Route('/exercise/{id}', name: 'individualExercise')]
+
+    public function show_exercise(ExerciseRepository $exerciseRepository, int $id): Response
+    {
+        $exercise = $exerciseRepository->find($id);
+
+        if (!$exercise) {
+            throw $this->createNotFoundException(
+                'No exercise found for id' . $id
+            );
+        }
+        return $this->render('Exercises\individual_exercise.html.twig', [
+            'exercise' => $exercise
+        ]);
+    }
+
+
+    #[Route('/exercise-form',name: 'exercise_form', methods: array('GET', 'POST'))]
+
+    public function exercise_form(Request $request, EntityManagerInterface $entityManager): Response{
+        $exercise = new Exercise();
+        $form = $this->createForm(ExerciseType::class, $exercise);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $exercise = $form->getData();
+            $tip = new Tip();
+            $tip->setNume("uuas");
+            $exercise->setTip($tip);
+            $entityManager->persist($exercise); #pregateste codul
+            $entityManager->persist($tip);
+            $entityManager->flush(); #il ruleaza
+
+        }
+
+        return $this->render('Exercises\addExercisePage.html.twig', ['form' => $form, ]);
+    }
+
+
 }
